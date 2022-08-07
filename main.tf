a0febee7a920bd2ee95b654837b6ac1cf7b43b44
unknown <rohitgarg11@outlook.com>
2022-08-07 21:50:11 +0530

    ec2 with user data

--- main.tf
+++ main.tf
@@ -7,11 +7,13 @@ resource "aws_instance" "tf-first-instance" {
     instance_type = "t2.micro"  
     vpc_security_group_ids = [aws_security_group.tf-sg-1.id]
 
-    user_data = <<-EOF
-        #!/bin/bash
-        echo "Hello World">index.html
-        nohup busybox httpd -f -p 8080 &
-        EOF
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
+
+  user_data_replace_on_change = true
 
     tags = {
       "Name" = "tf-1"
@@ -22,9 +24,20 @@ resource "aws_security_group" "tf-sg-1" {
     name = "terraform-sg-1"
 
     ingress {
-        from_port = 8080
-        to_port = 8080
+        from_port = var.server_port
+        to_port = var.server_port
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
-    }  
+    }
+}
+
+variable "server_port" {
+  type = number
+  default = 8080
+  description = "server port to listen on"
+}
+
+output "ec2_publicip" {
+  value = "aws_instance.tf_first_instance.public_ip"
+  description = "public ip of launched EC2 instance"
 }

aa0012196e9c10badaee29878c19087fd0664ddc
unknown <rohitgarg11@outlook.com>
2022-08-07 14:27:54 +0530

    ec2 instance with security group

--- main.tf
+++ main.tf
@@ -1,2 +1,30 @@
 provider "aws" {  
+    region = "ap-south-1"
+}
+
+resource "aws_instance" "tf-first-instance" {
+    ami = "ami-0325e3016099f9112"
+    instance_type = "t2.micro"  
+    vpc_security_group_ids = [aws_security_group.tf-sg-1.id]
+
+    user_data = <<-EOF
+        #!/bin/bash
+        echo "Hello World">index.html
+        nohup busybox httpd -f -p 8080 &
+        EOF
+
+    tags = {
+      "Name" = "tf-1"
+    }
+}
+
+resource "aws_security_group" "tf-sg-1" {
+    name = "terraform-sg-1"
+
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }  
 }
